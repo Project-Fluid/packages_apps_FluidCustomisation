bb42073dcc21a87a2a7e17aeb8f0f589df9fb12f
Androbuddy <anirudhchimpidi8@gmail.com>
2020-10-07 17:47:55 +0530

    FluidCustomisation: Initialze Customisation App
    AndroBuddy:
    - Use Bottom Navigation bar logic from https://github.com/AospExtended/platform_packages_apps_Extensions/commit/982031c654565cb3343364777901c32b73676fb7#
    - Add a highlight effect
    - Update colors to match fluid
    - Add navigation section
    
    Elluzion:
    - Updated/fixed bottom bar logic is taken from https://github.com/Bootleggers-BrokenLab/packages_apps_bootlegdumpster/commit/89e2fe96bbc94009c7113c26c21d23d95895ec00
    
    Signed-off-by: Androbuddy <anirudhchimpidi8@gmail.com>
    Co-authored-by: Shubham Singh <coolsks94@gmail.com>
    Co-authored-by: Alex Cruz <du.alexcruz@gmail.com>
    Co-authored-by: Elluzion <dyrex2004@gmail.com>

---
+++ src/com/fluid/customisation/FluidCustomisation.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2014-2016 The Dirty Unicorns Project
+ * Copyright (C) 2020 ProjectFluid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fluid.customisation;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.Surface;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
+import android.preference.Preference;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.android.settings.SettingsPreferenceFragment;
+
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceManager;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.R;
+
+import com.fluid.customisation.categories.Themes;
+import com.fluid.customisation.categories.StatusBar;
+import com.fluid.customisation.categories.QS;
+import com.fluid.customisation.categories.Navigation;
+import com.fluid.customisation.categories.Extras;
+
+public class FluidCustomisation extends SettingsPreferenceFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.layout_fluid_customisation, container, false);
+        final BottomNavigationView bottomNavigation = (BottomNavigationView) view.findViewById(R.id.bottom_navigation);
+
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                if (item.getItemId() == bottomNavigation.getSelectedItemId()) {
+                    return false;
+                } else {
+                    if (item.getItemId() == R.id.navigation_themes) {
+                        switchFragment(new Themes());
+
+                    } else if (item.getItemId() == R.id.navigation_statusbar) {
+                        switchFragment(new StatusBar());
+
+                    } else if (item.getItemId() == R.id.navigation_qs) {
+                        switchFragment(new QS());
+
+                    } else if (item.getItemId() == R.id.navigation_navigation) {
+                        switchFragment(new Navigation());
+
+                    } else if (item.getItemId() == R.id.navigation_extras) {
+                        switchFragment(new Extras());
+                    }
+                    return true;
+                }
+            }
+        });
+
+        setHasOptionsMenu(true);
+        bottomNavigation.setSelectedItemId(R.id.navigation_themes);
+        switchFragment(new Themes());
+        return view;
+    }
+
+    private void switchFragment(Fragment fragment) {
+        getFragmentManager().beginTransaction().replace(R.id.fragment_layout, fragment).commit();
+    }
+    
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.FLUID;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+}
