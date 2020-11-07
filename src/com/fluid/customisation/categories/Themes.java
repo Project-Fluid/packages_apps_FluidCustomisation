234e20ee8c75c2a696351e6b6db08ab6c03c8c50
SagarMakhar <sagarmakhar@gmail.com>
2020-11-07 18:21:26 +0000

    Add lockscreen clock options
    - while we don't ship themepicker, let's add these options here
    
    Change-Id: I4ada7d90bd439c79836061b0eb21143fc6e25d4c
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- src/com/fluid/customisation/categories/Themes.java
+++ src/com/fluid/customisation/categories/Themes.java
@@ -17,10 +17,15 @@
 
 package com.fluid.customisation.categories;
 
+import static android.os.UserHandle.USER_CURRENT;
+
 import android.content.ContentResolver;
 import android.content.Context;
+import android.os.Bundle;
 import android.os.SystemProperties;
 import androidx.preference.*;
+import android.provider.Settings;
+import androidx.preference.Preference.OnPreferenceChangeListener;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -36,24 +41,52 @@ import com.android.settings.R;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Themes extends DashboardFragment {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Themes extends DashboardFragment implements OnPreferenceChangeListener{
 
     public static final String TAG = "Themes";
 
+    private static final String CUSTOM_CLOCK_FACE = Settings.Secure.LOCK_SCREEN_CUSTOM_CLOCK_FACE;
+    private static final String DEFAULT_CLOCK = "com.android.keyguard.clock.DefaultClockController";
+
     private ContentResolver mResolver;
+
+    private ListPreference mLockClockStyles;
+    private Context mContext;
 
     @Override
     public int getMetricsCategory() {
         return MetricsEvent.FLUID;
     }
 
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mContext = getActivity();
+
+        mLockClockStyles = (ListPreference) findPreference(CUSTOM_CLOCK_FACE);
+        String mLockClockStylesValue = getLockScreenCustomClockFace();
+        mLockClockStyles.setValue(mLockClockStylesValue);
+        mLockClockStyles.setSummary(mLockClockStyles.getEntry());
+        mLockClockStyles.setOnPreferenceChangeListener(this);
+    }
+    
     @Override
     protected String getLogTag() {
         return TAG;
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mLockClockStyles) {
+            setLockScreenCustomClockFace((String) newValue);
+            int index = mLockClockStyles.findIndexOfValue((String) newValue);
+            mLockClockStyles.setSummary(mLockClockStyles.getEntries()[index]);
+            return true;
+        }
         return false;
+
     }
 
     @Override
@@ -76,6 +109,30 @@ public class Themes extends DashboardFragment {
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.adaptive_icon_shape"));
         return controllers;
+    }
+
+    private String getLockScreenCustomClockFace() {
+        String value = Settings.Secure.getStringForUser(mContext.getContentResolver(),
+                CUSTOM_CLOCK_FACE, USER_CURRENT);
+
+        if (value == null || value.isEmpty()) value = DEFAULT_CLOCK;
+
+        try {
+            JSONObject json = new JSONObject(value);
+            return json.getString("clock");
+        } catch (JSONException ex) {
+        }
+        return value;
+    }
+
+    private void setLockScreenCustomClockFace(String value) {
+        try {
+            JSONObject json = new JSONObject();
+            json.put("clock", value);
+            Settings.Secure.putStringForUser(mContext.getContentResolver(), CUSTOM_CLOCK_FACE,
+                    json.toString(), USER_CURRENT);
+        } catch (JSONException ex) {
+        }
     }
 }
 
