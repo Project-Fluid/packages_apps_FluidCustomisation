userbybot/android_packages_apps_ConfigCenter

ConfigCenter: AccentColorPicker: set color preview from resources if default RGB value is set
Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>
Signed-off-by: Joey Huab <joey@evolution-x.org>

src/com/exui/config/center/display/AccentColorPreferenceController.java
@@ -65,6 +65,9 @@ public class AccentColorPreferenceController extends AbstractPreferenceControlle
             mAccentColor.setSummary(hexColor);
         }
         mAccentColor.setNewPreviewColor(intColor);
+        if (hexColor.equals("#ff1a73e8"))
+            mAccentColor.setCustomColorPreview(mContext.getResources()
+                    .getColor(R.color.evolution_icon_tint_color));
     }
 
     @Override

Project-Fluid/packages_apps_FluidCustomisation

src/com/fluid/customisation/etc/AccentColorPreferenceController.java
@@ -65,6 +65,9 @@ public class AccentColorPreferenceController extends AbstractPreferenceControlle
             mAccentColor.setSummary(hexColor);
         }
         mAccentColor.setNewPreviewColor(intColor);
+        if (hexColor.equals("#ff72bbff"))
+            mAccentColor.setCustomColorPreview(mContext.getResources()
+                    .getColor(R.color.fluid_default_accent_color));
     }
 
     @Override