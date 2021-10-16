ba8cec95712da93426d6cf4db980572a29d459ae
Elluzion <dyrex2004@gmail.com>
2021-10-16 21:11:44 +0200

    FluidCustomisation: Initialise
    This empty Customisation app is written in Kotlin
    and acts as an extension of packages/apps/Settings.

---
+++ src/org/projectfluid/customisation/FluidCustomisation.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2021 Project Fluid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.projectfluid.customisation
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent
+
+import android.os.Bundle
+
+import androidx.preference.Preference
+
+import com.android.settings.R
+import com.android.settings.SettingsPreferenceFragment
+
+class FluidCustomisation : SettingsPreferenceFragment(), Preference.OnPreferenceChangeListener {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.customisation_main, rootKey)
+    }
+
+    override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
+        return true
+    }
+
+    override fun getMetricsCategory(): Int = MetricsEvent.FLUID
+
+    companion object {
+        const val TAG = "FluidCustomisation"
+    }
+}
