7c5ab32b9bd363ec54058a49878e5def3833ad14
Elluzion <dyrex2004@gmail.com>
2022-07-31 16:42:39 +0000

    FluidCustomisation: Misc
    Change-Id: I3ac5df1036766ec357429a5fbd289882ea30bcf3

--- src/org/projectfluid/customisation/preference/SystemThemePreference.kt
+++ src/org/projectfluid/customisation/preference/SystemThemePreference.kt
@@ -2,19 +2,16 @@ package org.projectfluid.customisation.preference
 
 import android.content.Context
 import android.util.AttributeSet
+import android.widget.Toast
 
 import org.projectfluid.ui.preference.FluidSystemThemePreferenceBase
 
 class SystemThemePreference(context: Context, attributeSet: AttributeSet)
     : FluidSystemThemePreferenceBase(context, attributeSet) {
 
-    // remove these comments later yes yes
     // open the fitting fragment for more settings (called on button press)
-    override fun openMonetOrThemeSettings() {
-        //when (monetEnabled) {
-        //    true -> Toast.makeText(context, "Open monet settings", Toast.LENGTH_SHORT).show()
-        //    false -> Toast.makeText(context, "Open theme settings", Toast.LENGTH_SHORT).show()
-        //}
+    override fun openThemeSettings() {
+        Toast.makeText(context, "Monet switch coming soon", Toast.LENGTH_SHORT).show()
     }
 
     // check state whether monet do be enabled
